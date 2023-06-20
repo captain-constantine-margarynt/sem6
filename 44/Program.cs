f04dab996ee1cd4357dcc0fe65104563c483a827
captain-constantine-margarynt <89223974+captain-constantine-margarynt@users.noreply.github.com>
2023-06-20 16:25:09 +0300

    44

---
+++ 44/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5-> 0 1 1 2 3
+
+// Если N = 3 -> 0 1 1
+
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// <aside>
+// ❗️ Числа Фибоначчи — числовая ****последовательность,
+// каждый элемент которой равен сумме двух предыдущих.
+
+Console.Clear();
+Console.WriteLine("Enter your number ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Your number is {num}");
+
+int [] result = fib(num);
+Console.WriteLine("Fibonnaci sequence: ");
+Console.WriteLine(String.Join(", ", result));
+
+int [] fib (int num) {
+    int [] fib = new int [num];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i=2; i < fib.Length; i++) {
+        fib[i] = fib[i - 1] + fib [i - 2];
+    }
+    return fib;
+}
